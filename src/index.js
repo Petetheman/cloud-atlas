f676d3729bba8e10c4910a8db47acd1d3ce3e81e
Petter <pierredenorvege@gmail.com>
2025-02-06 20:37:58 +0100

    added parameter parsing to routes

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@
 export class CloudAtlas {
 	constructor() {
 		this.routes = [];
-		this.current_group = null;
-		this.current_route = null;
-		this.current_chain = null;
+		this._set();
 	}
+
+    _set(group=null, route=null, chain=null) {
+        this.current_route = route;
+        this.current_group = group;
+        this.current_chain = chain;
+        return this;
+    }
 
 	group(path) {
 		const group = { path, bmw: [], error: [], amw: []};
-		this.current_route = null;
-		this.current_group = group;
-		this.current_chain = group.bmw;
-		return this;
+		return this._set(group, null, group.bmw);
 	}
 
 	on(method, path) {
 		const route = {path: (this.current_group?.path || '') + path, method, group: this.current_group, mw: [], error: this.current_group?.error || []};
 		this.routes.push(route);
-		this.current_route = route;
-		this.current_chain = route.mw;
-		return this;
+		return this._set(route.group, route, route.mw);
 	}
 
 	onError(error, status, message) {
@@ -29,16 +29,13 @@ export class CloudAtlas {
 		return this;
 	}
 
-	beforeAll() {this.current_route = null; this.current_chain = this.current_group.bmw; return this;}
+	beforeAll() {return this._set(this.current_group, null, this.current_group.bmw)}
 
-	afterAll() {this.current_route = null; this.current_chain = this.current_group.amw; return this;}
+	afterAll() {return this._set(this.current_group, null, this.current_group.bmw)}
 
 	use(mw) {
-		if (Array.isArray(mw)) {
-			mw.forEach(m => this.use(m));
-		}
+		if (Array.isArray(mw)) mw.forEach(m => this.use(m));
 		this.current_chain.push(mw);
-
 		return this;
 	}
 
@@ -49,25 +46,27 @@ export class CloudAtlas {
 		if (!route) return new Response('Not found', { status: 404 });
 		
 		try {
-			const middlewares = [...route.group.bmw, ...route.mw];
-			const executor = this.compose(middlewares);
-			await executor(con);
+			await this.compose([...route.group.bmw, ...route.mw])(con);
 		} catch (err) {
 			const handler = [...route.error, ...route.group.error].find(h => err instanceof h.error);
 			if (!handler) throw err;
-			con.status = handler.status;
-			con.body = handler.message+' - '+err.message;
-			con.statusText = handler.message;
+            con.res = {...con.res, status: handler.status, body: handler.message+' - '+err.message, statusText: handler.message};
 		} finally {
 			for (let mw of route.group.amw) await mw(con, () => Promise.resolve());
 		}
-		return con.response();
+		return con.res();
 	}
 
 	matches(route, con) {
-		const reqParts = con.path.split('/').filter(Boolean);
-		const routeParts = route.path.split('/').filter(Boolean);		
-		return 	con.method === route.method && reqParts.length === routeParts.length && routeParts.every((p, i) => p.startsWith(':') || p === reqParts[i]);
+		const [reqParts, routeParts] = [con.path, route.path].map(p => p.split('/').filter(Boolean));
+		
+		if (con.method !== route.method || reqParts.length !== routeParts.length) return false;
+		
+		con.params = routeParts.reduce(
+            (acc, p, i) => {if (p.startsWith(':')) acc[p.slice(1)] = reqParts[i];return p.startsWith(':') || p === reqParts[i] ? acc : null;},
+            {});
+		
+		return !!con.params;
 	}
 
 	compose(middlewares) {
@@ -83,20 +82,17 @@ export class Context {
 		this.req = req;
 		this.env = env;
 		this.ctx = ctx;
-		this.headers = {};
-		this._body = {};
-		this.status = 200;
-		this.statusText = 'OK';
+        this.params = {};
+        this.res = {_body: {}, status: 200, statusText: 'OK', headers: {}};
 	}
 
 	get path() { return new URL(this.req.url).pathname;}
 	get method() { return this.req.method; }
-	get body() { return this._body; }
-	set body(v) {this._body = v;}
+	get body() { return this.res._body; }
+	set body(v) {this.res._body = v;}
 
 	response() {
-		this.headers['Content-Type'] = typeof this._body === 'object' ? 'application/json' : 'text/plain';
-		let body = typeof this._body === 'object' ? JSON.stringify(this._body) : this._body;
-		return new Response(body, {status: this.status,headers: this.headers, statusText: this.statusText});
+		this.res.headers['Content-Type'] = typeof this.res._body === 'object' ? 'application/json' : 'text/plain';
+		return new Response(typeof this.res._body === 'object' ? JSON.stringify(this.res._body) : this.res._body, {status: this.res.status, headers: this.res.headers, statusText: this.res.statusText});
 	}
 }
