f39e32b0d78b8a496ed1144b93b023930a66abb2
Petter <pierredenorvege@gmail.com>
2025-02-07 08:10:57 +0100

    reduced to 61 lines

--- src/index.js
+++ src/index.js
@@ -14,9 +14,8 @@ export class CloudAtlas {
         return this.set(group, null, group.bmw);
     }
 
-    on(method, path) {
-        this.current.group ||= this.group('');
-        const rndx = this.routes.push({ path: (this.current.group.path || '') + path, method, group: this.current.group, mw: [], error: [] });
+    on(method, path) {        
+        const rndx = this.routes.push({path: (path=(group=(this.current.group.path || this.group(''))) + path), method, group: group, mw: [], error: [], pattern: new RegExp(`^${path.replace(/:(\w+)/g, (_, paramName) => `(?<${paramName}>[^/]+)`)}$`)});
         return this.set(this.current.group, this.routes[rndx], this.routes[rndx].mw);
     }
 
@@ -27,7 +26,7 @@ export class CloudAtlas {
 
     catch(error, status, message) {return this.catchAny(error, async (ex, con) => {con.res = { ...con.res, status: status, body: message, statusText: message }; });}
     beforeAll() { return this.set(this.current.group, null, this.current.group.bmw) }
-    afterAll() { return this.set(this.current.group, null, this.current.group.bmw) }
+    afterAll() { return this.set(this.current.group, null, this.current.group.amw) }
 
     use(mw) {
         Array.isArray(mw) ? mw.forEach(m => this.use(m)) : this.current.chain.push(mw);
@@ -35,34 +34,15 @@ export class CloudAtlas {
     }
 
     async handle(con) {
-        const route = this.routes.find(r => this.matches(r, con));
+        const route = this.routes.find(r => con.req.method === r.method && (con.params = r.pattern.exec(new URL(con.req.url).pathname)) ? true : false);
         if (!route) return new Response('Not found', { status: 404 });
-
-        try {
-            await this.compose([...route.group.bmw, ...route.mw])(con);
-        } catch (err) {
-            const handler = [...route.error, ...route.group.error].find(h => err instanceof h.error);
-            if (!handler) { throw err } else { await handler.method(err, con); }
-        } finally {
-            await this.compose(route.group.amw)(con);
-        }
+        try {await this.compose([...route.group.bmw, ...route.mw])(con);} 
+        catch (err) {(handler = [...route.error, ...route.group.error].find(h => err instanceof h.error)) ? await handler.method(err, con) : (() => { throw err; })()} 
+        finally {await this.compose(route.group.amw)(con)}
         return con.response();
     }
 
-    matches(route, con) {
-        const [reqParts, routeParts] = [con.path, route.path].map(p => p.split('/').filter(Boolean));
-        if (con.method !== route.method || reqParts.length !== routeParts.length) return false;
-        con.params = routeParts.reduce((acc, p, i) => { if (p.startsWith(':')) { acc[p.slice(1)] = reqParts[i]; return acc; } return p === reqParts[i] ? acc : null; }, {});
-        return !!con.params;
-    }
-
-    compose(middlewares) {
-        return async (ctx) => {
-            const dispatch = async (index) => { index < middlewares.length ? await middlewares[index](ctx, () => dispatch(index + 1)) : Promise.resolve() };
-            await dispatch(0);
-        };
-    }
-
+    compose(middlewares) {return async (ctx) => { await (dispatch = async (index) => { index < middlewares.length ? await middlewares[index](ctx, () => dispatch(index + 1)) : Promise.resolve() })(0)}}
     config() { return { fetch: async (req, env, ctx) => { return this.handle(new Context(req, env, ctx)) } }; }
 }
 
@@ -77,8 +57,5 @@ export class Context {
         this.params = {};
         this.res = { body: {}, status: 200, statusText: 'OK', headers: { "Content-Type": "application/json" } };
     }
-
-    get path() { return new URL(this.req.url).pathname; }
-    get method() { return this.req.method; }
     response() { return new Response(JSON.stringify(this.res.body), { status: this.res.status, headers: this.res.headers, statusText: this.res.statusText }) }
 }
