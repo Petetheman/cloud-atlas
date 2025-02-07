1256265fb6adbd53fd4ed3578ac1b411e3f337f1
Petter <pierredenorvege@gmail.com>
2025-02-07 08:26:20 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -14,8 +14,9 @@ export class CloudAtlas {
         return this.set(group, null, group.bmw);
     }
 
-    on(method, path) {        
-        const rndx = this.routes.push({path: (path=(group=(this.current.group.path || this.group(''))) + path), method, group: group, mw: [], error: [], pattern: new RegExp(`^${path.replace(/:(\w+)/g, (_, paramName) => `(?<${paramName}>[^/]+)`)}$`)});
+    on(method, path) {  
+        let group = (this.current.group||this.group(''));
+        const rndx = this.routes.push({path: group.path + path, method, group: group, mw: [], error: [], pattern: new RegExp(`^${(group.path+path).replace(/:(\w+)/g, (_, paramName) => `(?<${paramName}>[^/]+)`)}$`)})-1;
         return this.set(this.current.group, this.routes[rndx], this.routes[rndx].mw);
     }
 
@@ -23,27 +24,35 @@ export class CloudAtlas {
         (this.current.route || this.current.group).error.push({ error, method });
         return this;
     }
-
-    catch(error, status, message) {return this.catchAny(error, async (ex, con) => {con.res = { ...con.res, status: status, body: message, statusText: message }; });}
-    beforeAll() { return this.set(this.current.group, null, this.current.group.bmw) }
-    afterAll() { return this.set(this.current.group, null, this.current.group.amw) }
-
+    
     use(mw) {
         Array.isArray(mw) ? mw.forEach(m => this.use(m)) : this.current.chain.push(mw);
         return this;
     }
 
+    catch(error, status, message) {return this.catchAny(error, async (ex, con) => {con.res = { ...con.res, status: status, body: message, statusText: message }; });}
+    beforeAll() { return this.set(this.current.group, null, this.current.group.bmw) }
+    afterAll() { return this.set(this.current.group, null, this.current.group.amw) }
+    config() { return { fetch: async (req, env, ctx) => { return this.handle(new Context(req, env, ctx)) } }; }
+    
+
     async handle(con) {
         const route = this.routes.find(r => con.req.method === r.method && (con.params = r.pattern.exec(new URL(con.req.url).pathname)) ? true : false);
         if (!route) return new Response('Not found', { status: 404 });
         try {await this.compose([...route.group.bmw, ...route.mw])(con);} 
-        catch (err) {(handler = [...route.error, ...route.group.error].find(h => err instanceof h.error)) ? await handler.method(err, con) : (() => { throw err; })()} 
+        catch (err) {
+            let handler = [...route.error, ...route.group.error].find(h => err instanceof h.error) 
+            handler ? await handler.method(err, con) : (() => { throw err; })()
+        } 
         finally {await this.compose(route.group.amw)(con)}
         return con.response();
     }
 
-    compose(middlewares) {return async (ctx) => { await (dispatch = async (index) => { index < middlewares.length ? await middlewares[index](ctx, () => dispatch(index + 1)) : Promise.resolve() })(0)}}
-    config() { return { fetch: async (req, env, ctx) => { return this.handle(new Context(req, env, ctx)) } }; }
+    compose(middlewares) {return async (ctx) => { 
+        dispatch = async (index) => { index < middlewares.length ? await middlewares[index](ctx, () => dispatch(index + 1)) : Promise.resolve() }
+        await dispatch(0)
+    }}
+    
 }
 
 ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS', 'HEAD'].forEach(method => { CloudAtlas.prototype['on' + method] = function (path) { return this.on(method, path); }; });
